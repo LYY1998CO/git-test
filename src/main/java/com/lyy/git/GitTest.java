77127b3f00cd1709133109fda6cd08bc8a7f8209
LYY <2715604966@qq.com>
2022-02-17 12:24:53 +0800

    second commit

--- src/main/java/com/lyy/git/GitTest.java
+++ src/main/java/com/lyy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lyy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloGit");
+        System.out.println("HelloGitHub");
     }
 }
