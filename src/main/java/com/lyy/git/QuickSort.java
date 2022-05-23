3243af9bf2833b86aaa4f097136c553ac1f3bb31
LYY <2715604966@qq.com>
2022-05-23 09:57:54 +0800

    快速排序 Commit

--- src/main/java/com/lyy/git/QuickSort.java
+++ src/main/java/com/lyy/git/QuickSort.java
@@ -1,38 +1,40 @@
 package com.lyy.git;
+
 //快速排序实现
 public class QuickSort {
     public static void main(String[] args) {
-        int arr[]={5,4,4,5,2};
+        int arr[] = {5, 4, 4, 5, 2};
         System.out.println("排序前的数组:");
-        for (int i = 0; i <arr.length ; i++) {
-            System.out.print(arr[i]+"\t");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
         }
         QuickSort sort = new QuickSort();
-        sort.QuickSort(arr,0,arr.length-1);
+        sort.QuickSort(arr, 0, arr.length - 1);
         System.out.println("\n排序后的数组:");
-        for (int i = 0; i <arr.length ; i++) {
-            System.out.print(arr[i]+"\t");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "\t");
         }
     }
-    public void QuickSort(int arr[],int start,int end){
-        if(start<end)
-        {
-            int index=getIndex(arr,start,end);
-            QuickSort(arr,start,index-1);
-            QuickSort(arr,index+1,end);
+
+    public void QuickSort(int arr[], int start, int end) {
+        if (start < end) {
+            int index = getIndex(arr, start, end);
+            QuickSort(arr, start, index - 1);
+            QuickSort(arr, index + 1, end);
         }
     }
-    public int getIndex(int arr[],int start,int end){
-        int pivot=arr[start];
-        while (start<end){
-            while (arr[end]>=pivot&&start<end)
+
+    public int getIndex(int arr[], int start, int end) {
+        int pivot = arr[start];
+        while (start < end) {
+            while (arr[end] >= pivot && start < end)
                 end--;
-                arr[start]=arr[end];
-            while (arr[start]<=pivot&&start<end)
+            arr[start] = arr[end];
+            while (arr[start] <= pivot && start < end)
                 start++;
-                arr[end]=arr[start];
+            arr[end] = arr[start];
         }
-        arr[start]=pivot;
+        arr[start] = pivot;
         return start;
     }
 }
